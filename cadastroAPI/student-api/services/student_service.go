d9f8c9aff5526a51cbd938c90a67c48725236099
Pedro <pedrohenriquefa97@gmail.com>
2025-03-21 00:38:04 -0300

    commented

--- cadastroAPI/student-api/services/student_service.go
+++ cadastroAPI/student-api/services/student_service.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"sync"
 
-	"github.com/google/uuid" // Para gerar IDs únicos
+	"github.com/google/uuid"
 )
 
 type StudentService struct {
@@ -23,7 +23,7 @@ func (s *StudentService) CreateStudent(student models.Student) models.Student {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	student.ID = uuid.New().String() // Gerar um ID único
+	student.ID = uuid.New().String()
 	s.students[student.ID] = student
 	return student
 }
@@ -72,48 +72,3 @@ func (s *StudentService) GetAllStudents() []models.Student {
 	}
 	return studentList
 }
-
-// func (s *StudentService) GetStudent(id int) (models.Student, error) {
-// 	s.mu.Lock()
-// 	defer s.mu.Unlock()
-
-// 	student, exists := s.students[id]
-// 	if !exists {
-// 		return models.Student{}, errors.New("student not found")
-// 	}
-// 	return student, nil
-// }
-
-// func (s *StudentService) UpdateStudent(id int, student models.Student) (models.Student, error) {
-// 	s.mu.Lock()
-// 	defer s.mu.Unlock()
-
-// 	if _, exists := s.students[id]; !exists {
-// 		return models.Student{}, errors.New("student not found")
-// 	}
-// 	student.ID = id
-// 	s.students[id] = student
-// 	return student, nil
-// }
-
-// func (s *StudentService) DeleteStudent(id int) error {
-// 	s.mu.Lock()
-// 	defer s.mu.Unlock()
-
-// 	if _, exists := s.students[id]; !exists {
-// 		return errors.New("student not found")
-// 	}
-// 	delete(s.students, id)
-// 	return nil
-// }
-
-// func (s *StudentService) GetAllStudents() []models.Student {
-// 	s.mu.Lock()
-// 	defer s.mu.Unlock()
-
-// 	var studentList []models.Student
-// 	for _, student := range s.students {
-// 		studentList = append(studentList, student)
-// 	}
-// 	return studentList
-// }
