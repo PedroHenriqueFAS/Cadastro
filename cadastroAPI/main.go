fab74fe0c706bc24070a6c9945e4a2f1399c437c
Pedro <pedrohenriquefa97@gmail.com>
2025-03-21 00:06:37 -0300

    Update Go module dependencies and add CORS middleware to main application

--- cadastroAPI/main.go
+++ cadastroAPI/main.go
@@ -4,10 +4,19 @@ import (
 	"cadastroAPI/routes"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4200"}, // Permite requisições do front-end
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	routes.SetupRoutes(r)
 	r.Run(":8080")
 }
